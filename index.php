647e3a3b36ecf79a785abd204a4ff5c8d4334f84
Catalín Munteanu <catalinapampin@gmail.com>
2023-12-21 13:44:08 -0300

    updated index.php and scss files

--- index.php
+++ index.php
@@ -7,27 +7,50 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edra</title>
     <!-- Add any additional meta tags, link tags, or stylesheets below this line -->
+    <link rel="stylesheet" href="scss/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Archivo:wght@100;600&display=swap" rel="stylesheet">
 </head>
 
 <body>
     <header>
+        <img src="img/mobile/edra-logo-mobile.png">
+        <nav>
+            <button id="burger-menu-toggler" aria-label="Menú de navegación">
+                <img id="open-mobile-menu" src="img/mobile/burger-menu-open.png" alt="Botón de apertura del menú">
+            </button>
+        </nav>
     </header>
 
     <main>
         <section class="presentacion">
-            <img src="img/mobile/img-index.png">
-            <article>
-                <p>Lanzamos DESAFÍO CERO con el objetivo de validar nuestra solución y su impacto positivo en la cadena de producción de alimentos. Juntos a nuestros aliados buscamos colaborar en la resolución de dos grandes desafíos: la seguridad alimentaria y el cambio climático.</p>
-                <h4>Colaboramos a resolver estos dos problemas</h4>
-            </article>
+            <img id="presentacion-mobile" src="img/mobile/img-index.png">
+            <div class="article-container">
+                <article>
+                    <p>Lanzamos DESAFÍO CERO con el objetivo de validar nuestra solución y su impacto positivo en la cadena de producción de alimentos. Juntos a nuestros aliados buscamos colaborar en la resolución de dos grandes desafíos: la seguridad alimentaria y el cambio climático.</p>
+                    <h4>Colaboramos a resolver estos dos problemas</h4>
+                    <div class="container-problemas">
+                        <span id="icon-cambio-climatico"><img src="img/mobile/icon-cambio-climatico.png"></span>
+                        <span id="icon-seguridad-alimentaria"><img src="img/mobile/icon-seguridad-alimentaria.png"></span>
+                    </div>
+                </article>
+            </div>
         </section>
 
         <section class="objetivo-desarrollo-sostenible">
-            <article>
+            <article class="impacto-ods">
                 <h4>Impactamos en estos ODS
                     (Objetivo de Desarrollo Sostenible)</h4>
+                <div class="gallery-ods">
+                    <img src="img/mobile/ODS 2.png">
+                    <img src="img/mobile/ODS 12.png">
+                    <img src="img/mobile/ODS 13.png">
+                    <img src="img/mobile/ODS 15.png">
+                    <img src="img/mobile/ODS 17.png">
+                </div>
             </article>
-            <article>
+            <article class="desarrollo-ods">
                 <img src="img/mobile/logo-edra-body-mobile.png">
                 <p>En esta oportunidad buscamos  incorporar, monitorear y cuantificar el impacto de la implementación de cuatro Prácticas de Agricultura Sostenible en la campaña 23-24. Para esto, creemos que es fundamental la articulación de diferentes actores de la cadena de suministro.</p>
                 <p>Por tal motivo convocamos a 5 productores y 5 empresas. Desde EDRA creemos que es sumamente importante empezar a establecer relaciones de colaboración a largo plazo para compartir esfuerzos y beneficios que aceleren la transición a modelos productivos sustentables por medio de la tecnología.</p>
@@ -35,7 +58,46 @@
         </section>
 
         <section class="propuestas-desafio-cero">
-            <h5>Por eso en el DESAFÍO CERO nos propusimos</h5>
+            <article class="container-title">
+                <img src="img/mobile/icon-dardo-mobile.png">
+                <h5>Por eso en el DESAFÍO CERO nos propusimos</h5>
+            </article>
+            <article class="container-propuestas">
+                <div class="propuestas-implementar">
+                    <p>Implementar</p>
+                    <div class="container-prop-implementar">
+                        <figure>
+                            <img src="img/mobile/icon-conservacion-mobile.png">
+                            <figcaption>10 hectáreas de áreas de conservación</figcaption>
+                        </figure>
+                        <figure>
+                            <img src="img/mobile/icon-cultivo-mobile.png">
+                            <figcaption>500 hectáreas de cultivo de cobertura</figcaption>
+                        </figure>
+                    </div>
+                </div>
+                <div class="propuestas-recuperar">
+                    <p>Recuperar</p>
+                    <div class="container-prop-recuperar">
+                        <figure>
+                            <img src="img/mobile/icon-alimentos-mobile.png">
+                            <figcaption>125 toneladas de alimentos</figcaption>
+                        </figure>
+                        <figure>
+                            <img src="img/mobile/icon-plasticos-mobile.png">
+                            <figcaption>2.500 kilos de plásticos</figcaption>
+                        </figure>
+                    </div>
+                </div>
+            </article>
+        </section>
+
+        <section class="card-video">
+            <h4>EDRA es una climatech que promueve la implementación de soluciones basadas en la naturaleza a través de herramientas tecnológicas</h4>
+            <article>
+                <h4>LANZAMOS EL DESAFÍO CERO</h4>
+                <p>Te contamos sobre nuestra metodología y nuestra visión</p>
+            </article>
         </section>
 
         <section class="practicas-agricultura-sostenible">
