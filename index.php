8f51c0ddaeec331a95edf88df7f54935657b64ec
Catalín Munteanu <catalinapampin@gmail.com>
2023-12-21 10:45:23 -0300

    created index.php and img folder

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edra</title>
+    <!-- Add any additional meta tags, link tags, or stylesheets below this line -->
+</head>
+
+<body>
+    <header>
+    </header>
+
+    <main>
+        <section class="presentacion">
+            <img src="img/mobile/img-index.png">
+            <article>
+                <p>Lanzamos DESAFÍO CERO con el objetivo de validar nuestra solución y su impacto positivo en la cadena de producción de alimentos. Juntos a nuestros aliados buscamos colaborar en la resolución de dos grandes desafíos: la seguridad alimentaria y el cambio climático.</p>
+                <h4>Colaboramos a resolver estos dos problemas</h4>
+            </article>
+        </section>
+
+        <section class="objetivo-desarrollo-sostenible">
+            <article>
+                <h4>Impactamos en estos ODS
+                    (Objetivo de Desarrollo Sostenible)</h4>
+            </article>
+            <article>
+                <img src="img/mobile/logo-edra-body-mobile.png">
+                <p>En esta oportunidad buscamos  incorporar, monitorear y cuantificar el impacto de la implementación de cuatro Prácticas de Agricultura Sostenible en la campaña 23-24. Para esto, creemos que es fundamental la articulación de diferentes actores de la cadena de suministro.</p>
+                <p>Por tal motivo convocamos a 5 productores y 5 empresas. Desde EDRA creemos que es sumamente importante empezar a establecer relaciones de colaboración a largo plazo para compartir esfuerzos y beneficios que aceleren la transición a modelos productivos sustentables por medio de la tecnología.</p>
+            </article>
+        </section>
+
+        <section class="propuestas-desafio-cero">
+            <h5>Por eso en el DESAFÍO CERO nos propusimos</h5>
+        </section>
+
+        <section class="practicas-agricultura-sostenible">
+            <h4>PRÁCTICAS DE AGRICULTURA SOSTENIBLE</h4>
+            <article class="cultivo-cobertura"></article>
+            <article class="areas-conservacion"></article>
+            <article class="gestion-perdidas-desperdicios"></article>
+            <article class="recuperacion-envases"></article>
+        </section>
+
+        <section class="aliados-y-objetivos">
+            <h3>ALIADOS DEL DESAFÍO CERO</h3>
+            <h4>Productores y empresas que se sumaron a esta prueba cerrada.</h4>
+            <h3>OBJETIVOS DEL DESAFÍO CERO</h3>
+            <h4>En 250 días nos propusimos alcanzar:</h4>
+        </section>
+
+        <section class="indicadores-impacto">
+            <img src="img/mobile/indicadores-impacto-mobile.png">
+        </section>
+    </main>
+
+    <footer>
+
+        <article class="subscribe-form">
+            <h3>Suscribite a nuestro Newsletter</h3>
+            <p>Recibí todas las novedades del programa Desafío Cero</p>
+            <form>
+
+            </form>
+        </article>
+        <img src="img/mobile/logo-desafio-cero-footer.png">
+        <h4>DESAFÍO CERO</h4>
+        <p>Powered by ALT</p>
+    </footer>
+
+</body>
+
+</html>
